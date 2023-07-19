47ea017fa0caac8d427c321ed58bf942be125cd0
Alex <alexandershaw4@gmail.com>
2023-07-19 14:12:48 +0100

    yes

--- PSILO/computereducedoperator.m
+++
@@ -1,7 +0,0 @@
-function V = computereducedoperator(M)
-
-[~,ix] = find(sum(M));
-V = sparse(1:length(ix),(ix),1,length(ix),size(M,2));
-
-
-end
--- aconnectivity/+aconnectivity/computereducedoperator.m
+++
@@ -1,8 +0,0 @@
-function V = computereducedoperator(M,fun)
-% V = computereducedoperator(M)
-% AS2023
-
-if nargin < 2 || isempty(fun); fun = @sum; end
-
-ix = find(fun(M));
-V = sparse(1:length(ix),(ix),1,length(ix),size(M,2));
